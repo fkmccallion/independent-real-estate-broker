8365dc31c8da5a34340dc7ff74e9e3c349288d3b
github username <github email address>
2020-03-31 15:16:55 -0700

    update properties controller and serializer

--- api/app/controllers/properties_controller.rb
+++ api/app/controllers/properties_controller.rb
@@ -1,2 +1,12 @@
 class PropertiesController < ApplicationController
+
+  def index
+    properties = Property.all
+    render json: PropertySerializer.new(properties).to_serialized_json
+  end
+
+  def show
+    property = Property.find_by(id: params[:id])
+    render json: PropertySerializer.new(property).to_serialized_json
+  end
 end
