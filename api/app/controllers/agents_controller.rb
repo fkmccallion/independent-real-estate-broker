33ed2b9427a34069040550e28baa5ebbfb32344a
github username <github email address>
2020-03-31 14:13:43 -0700

    update agents controler and serializer

--- api/app/controllers/agents_controller.rb
+++ api/app/controllers/agents_controller.rb
@@ -1,2 +1,13 @@
 class AgentsController < ApplicationController
+
+  def index
+    agents = Agent.all
+    render json: AgentSerializer.new(agents).to_serialized_json
+  end
+
+  def show
+    agent = Agent.find_by(id: params[:id])
+    render json: AgentSerializer.new(agent).to_serialized_json
+  end
+
 end
