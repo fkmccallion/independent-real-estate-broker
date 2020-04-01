f89e3f63df1f4d9597218ae38e47cc8a2da580d1
github username <github email address>
2020-04-01 12:17:56 -0700

    add uncached files

---
+++ api/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :open_houses
+  resources :images
+  resources :property_images
+  resources :properties
+  resources :agents
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
