06e9f25d861ae1b0089ee2999d17982d6f9f6a63
github username <github email address>
2020-04-08 12:18:41 -0700

    update agent info

--- src/components/agents/Agent.js
+++ src/components/agents/Agent.js
@@ -5,9 +5,18 @@ const Agent = ({match, agents}) => {
 
   return (
     <div>
+
       <img className="agent-image" src={ agents[match.params.agentId].img_url ?  agents[match.params.agentId].img_url : "https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e" } alt={`${agents[match.params.agentId].first_name} ${agents[match.params.agentId].last_name} ${agents[match.params.agentId].bre_number}`} />
       <p>
-        <b>{agents[match.params.agentId].first_name + " " + agents[match.params.agentId].last_name}</b>
+        <b>{agents[match.params.agentId].first_name + " " + agents[match.params.agentId].last_name}</b><br />
+        <i>BRE Number:</i> {agents[match.params.agentId].bre_number}
+      </p>
+      <p>
+        {agents[match.params.agentId].phone}<br />
+        <a href={`mailto:${agents[match.params.agentId].email}`}>{agents[match.params.agentId].email}</a><br />
+      </p>
+      <p>
+        <i>{agents[match.params.agentId].biography}</i>
       </p>
 
     </div>
--- src/components/agents/NavbarAgents.js
+++ src/components/agents/NavbarAgents.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 class NavbarAgents extends React.Component {
 
   render() {
+    
     const renderAgents = Object.keys(this.props.agents).map(agentID =>
       <NavLink to={`/agents/${agentID}`}><img className="agent-image" src={this.props.agents[agentID].img_url} /></NavLink>
     );
