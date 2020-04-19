4715534806d1a34d2f771d63a1a8e11bac29c5b3
github username <github email address>
2020-04-19 09:04:19 -0700

    update agent images

--- src/components/agents/Agent.js
+++ src/components/agents/Agent.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Image from 'react-bootstrap/Image'
+
 const Agent = agent => {
   return (
     <div className="agent-card">
-      <img className="agent-image"  src={ agent.agent.img_url ?  agent.agent.img_url : "https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e" } alt={`${agent.agent.first_name} ${agent.agent.last_name} ${agent.agent.bre_number}`} />
+      <Image roundedCircle className="agent-image"  src={ agent.agent.img_url ?  agent.agent.img_url : "https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e" } alt={`${agent.agent.first_name} ${agent.agent.last_name} ${agent.agent.bre_number}`} />
       <span className="agent-contact-info">
           <b>{agent.agent.first_name + " " + agent.agent.last_name}</b><br />
           BRE Number: {agent.agent.bre_number}
