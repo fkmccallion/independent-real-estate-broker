17b090a53a496e1d3728404b930e0eb645a60978
github username <github email address>
2020-04-19 08:44:34 -0700

    add bootstrap cdn style

--- src/containers/Agents.js
+++ src/containers/Agents.js
@@ -7,6 +7,8 @@ import Agent from '../components/agents/Agent';
 
 import '../agents.css';
 
+import Carousel from 'react-bootstrap/Carousel';
+
 
 class Agents extends Component {
 
@@ -18,6 +20,46 @@ class Agents extends Component {
     return(
       <div>
         {this.props.agents.map(agent => <Agent agent={agent} />)}
+        <Carousel interval="100">
+          <Carousel.Item>
+            <img
+              width="250px"
+              className="d-block w-100"
+              src="https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="250"
+              className="d-block w-100"
+              src="https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              width="250"
+              className="d-block w-100"
+              src="https://firebasestorage.googleapis.com/v0/b/independent-real-estate-broker.appspot.com/o/images%2Fsemper-fi.gif?alt=media&token=d7da8996-c21f-40c0-843d-c7e26a4a688e"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
       </div>
     )
   }
