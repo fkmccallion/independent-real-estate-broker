bd1124e982303ce8d487a8bb80c70760ed66be3f
github username <github email address>
2020-04-29 13:38:57 -0700

    add login controller

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import agents from './agents';
 import properties from './properties';
 import images from './images';
 import testimonials from './testimonials'
+import login from './login'
 
 export default combineReducers({
   agents,
   properties,
   images,
-  testimonials
+  testimonials,
+  login
 });
